4797e10083fc7b496b277ea7395b40cd3ca1d00c
Oluwatobi <amure387@gmail.com>
2023-02-27 22:34:29 +0100

    fix: solved malloc issues

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,8 @@
 #include "binary_trees.h"
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp = malloc(sizeof(temp));
-	if (!temp)
-		return (NULL);
-	else
+	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
+	if (temp)
 	{
 		temp->n = value;
 		temp->parent = parent;
