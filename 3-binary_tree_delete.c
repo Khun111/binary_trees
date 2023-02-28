9579dae72da595395e8bb3b7ecd1cb52c96c1c44
Oluwatobi <amure387@gmail.com>
2023-02-28 10:22:00 +0100

    fix: task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,9 +5,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
-		return;
-	binary_tree_delete(tree->left);
-	binary_tree_delete(tree->left);
-	free(tree);
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
