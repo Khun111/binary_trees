afbb677bbc820156c214d441de51685801e7c06f
Oluwatobi <amure387@gmail.com>
2023-02-28 21:05:17 +0100

    push

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree->left)
 		{
-			j = 1 + binary_tree_height(tree->left);
+			i = 1 + binary_tree_height(tree->left);
 		}
 		else if (tree->right)
 		{
