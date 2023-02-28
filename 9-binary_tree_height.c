19fc6d1082fbfe2d8961c02253e5a99a5ac1b76a
Oluwatobi <amure387@gmail.com>
2023-02-28 21:12:09 +0100

    work

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,10 +6,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t i = 0, j = 0;
 
 	if (tree)
 	{
+		size_t i = 0, j = 0;
+
 		if (tree->left)
 		{
 			i = 1 + binary_tree_height(tree->left);
