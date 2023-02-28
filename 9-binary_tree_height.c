fc8fb7f82dbb5b135f6969da336c75459b2868ab
Oluwatobi <amure387@gmail.com>
2023-02-28 21:18:08 +0100

    fed up

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t i = 0, j = 0;
 
-		if (tree->left)
-		{
-			i = 1 + binary_tree_height(tree->left);
-		}
-		else if (tree->right)
-		{
-			j = 1 + binary_tree_height(tree->right);
-		}
+		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return (i > j ? i : j);
 	}
 	return (0);
