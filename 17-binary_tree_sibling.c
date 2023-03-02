6cc6f1f3c81f5faa979c3a17b88b1961ef398051
Oluwatobi <amure387@gmail.com>
2023-03-02 13:02:37 +0100

    fear: done 17 and passed betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: node pointer
+ * Return: node sibling
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node && node->parent)
